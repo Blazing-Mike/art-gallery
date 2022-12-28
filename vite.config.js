b7224fba6bf73c4c79df227241445c5e1d724ebc
Michael Adebambo <adebambomichael5@gmail.com>
2022-12-29 00:26:05 +0100

    build  output changes

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import path from 'path'
+
+export default {
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'location.html')
+      }
+    }
+  }
+}
